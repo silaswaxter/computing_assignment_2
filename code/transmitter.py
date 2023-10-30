12fafadd370ab432cda266ead4c2513545db35be
Silas Waxter <waxters@oregonstate.edu>
2023-10-29 18:08:24 -0700

    feat: add method to find FIR of root rasied cosine

--- code/transmitter.py
+++ code/transmitter.py
@@ -38,12 +38,26 @@ def GetSqrtRaisedCosineDTFT(w):
 
     # k = np.linspace(0, (n_samples - 1), num=n_samples)
 
+
 # Step 2: find the sqrt_raised_cosine impulse_response_function given the DFT
 #
 # h_sqrt_raised_cosine[n] = inverseDFT(H_sqrt_raised_cosine(k))
 #                         = inverseDFT(H_sqrt_raised_cosine(e^jw)), where w = (2*pi*k)/N
 #                                                                   where k = 0, 1, 2, ... N-1
 # NOTE: second half of inverseDFT samples must be shifted all the way to the beginning
+def GetSqrtRaisedCosineDFT(n_samples):
+    k = np.linspace(
+        (n_samples / -2), ((n_samples / 2) - 1), num=n_samples
+    )
+    w = (2 * np.pi * k) / n_samples
+    ifft = np.fft.ifft(GetSqrtRaisedCosineDTFT(w))
+    return np.fft.ifftshift(ifft)
+
+
+# Step 3: truncate the infinite impulse response h so that it can be an FIR filter
+def GetFIR():
+    filter_sample_count = 32
+    return GetSqrtRaisedCosineDFT(filter_sample_count)
 
 
 # Quick and dirty tests
@@ -63,5 +77,28 @@ def TEST_PlotHDTFT():
     plt.show()
 
 
+def TEST_PlotImpulseResponseFunction():
+    test_n_samples = 100
+    k = np.linspace(
+        (test_n_samples / -2), ((test_n_samples / 2) - 1), num=test_n_samples
+    )
+    test_h = GetSqrtRaisedCosineDFT(test_n_samples)
+
+    plt.plot(k, test_h.real, ".-")
+    plt.plot(k, test_h.imag, ".-") # imaginary parts should be zero 
+    plt.show()
+
+def TEST_PlotFIR():
+    test_n_samples = 32
+    k = np.linspace(0, (test_n_samples - 1), num=test_n_samples)
+    test_h_fir = GetFIR()
+
+    plt.plot(k, test_h_fir.real, ".:")
+    plt.show()
+
+
 if __name__ == "__main__":
-    TEST_PlotHDTFT()
+    # TEST_PlotHDTFT()
+    # TEST_PlotImpulseResponseFunction()
+    TEST_PlotFIR()
+
